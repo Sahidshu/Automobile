e7c13b9cacb5216b75074fac0f096208ea615f9f
Sahidshu <sahidmunoz0@gmail.com>
2024-11-30 15:18:25 -0500

    Nuevo proyecto

---
+++ src/main/java/espoch/edu/ec/automobileconcept/AutomobileConcept.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.automobileconcept;
+
+/**
+ *
+ * @author sahid
+ */
+public class AutomobileConcept {
+
+    public static void main(String[] args) {
+      
+    }
+}
