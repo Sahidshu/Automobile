c9c1b6fe394a19fb7a3d51c5ccb26affb25d0f75
CristhiAn-555 <cristhianvalencia999@gmail.com>
2024-11-30 19:24:43 -0500

    subi un nuevo proyecto

--- src/main/java/espoch/edu/ec/automobileconcept/classes/AutomobileInfo.java
+++ src/main/java/espoch/edu/ec/automobileconcept/classes/AutomobileInfo.java
@@ -15,6 +15,74 @@ public class AutomobileInfo {
     private int doorNum;
     private double currentV;
     private double maxV;
+    private String stripe;
+    private String mazda;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public int getDoorNum() {
+        return doorNum;
+    }
+
+    public void setDoorNum(int doorNum) {
+        this.doorNum = doorNum;
+    }
+
+    public double getCurrentV() {
+        return currentV;
+    }
+
+    public void setCurrentV(double currentV) {
+        this.currentV = currentV;
+    }
+
+    public double getMaxV() {
+        return maxV;
+    }
+
+    public void setMaxV(double maxV) {
+        this.maxV = maxV;
+    }
+
+    public String getStripe() {
+        return stripe;
+    }
+
+    public void setStripe(String stripe) {
+        this.stripe = stripe;
+    }
+
+    public String getMazda() {
+        return mazda;
+    }
+
+    public void setMazda(String mazda) {
+        this.mazda = mazda;
+    }
     
-   
+        
+    
+
 }
